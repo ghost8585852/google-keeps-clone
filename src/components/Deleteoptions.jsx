2dc77740864c23b5dad3bf7520e8fe2f0f81fbad
Harsh <panwarharsh432@gmail.com>
2026-08-25 22:59:48 +0530

    updated media tags for different screens

--- src/components/Deleteoptions.jsx
+++ src/components/Deleteoptions.jsx
@@ -3,17 +3,17 @@ import "./styles/deleteoption.css";
 export function Deleteiptions(props){
     return(
         <div className="deleteoptions-container">
-
+            <div className="buttons-container">
+                <button className="delbutton-style" onClick={(e)=>{e.stopPropagation() , props.delNotes()}} >Delete</button>
+                <button className="delbutton-style" onClick={(e)=>{e.stopPropagation() , props.recycleNotes()}} >Recycle</button>
+            </div>
             <div className="checkbutton-container">
                 <p>Select All</p>
                 <input className="checkboxx" onClick={(e)=>{e.stopPropagation()} }  onChange={props.selectAll} checked={props.selectreset} type="checkbox"/>
             </div>
 
 
-            <div className="buttons-container">
-                <button className="delbutton-style" onClick={(e)=>{e.stopPropagation() , props.delNotes()}} >Delete</button>
-                <button className="delbutton-style" onClick={(e)=>{e.stopPropagation() , props.recycleNotes()}} >Recycle</button>
-            </div>
+           
             
         </div>
         )
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -4,6 +4,7 @@ import LightbulbIcon from '@mui/icons-material/Lightbulb';
 import PushPinIcon from '@mui/icons-material/PushPin';
 import ArchiveIcon from '@mui/icons-material/Archive';
 import AutoDeleteIcon from '@mui/icons-material/AutoDelete';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
  function Sidebar(props){
 
     const [clickStyle, setClickStyle] = useState(true);
@@ -18,7 +19,8 @@ import AutoDeleteIcon from '@mui/icons-material/AutoDelete';
         setClickStyle(false);
     }
     return(
-        <div className="side-bar">
+        <div className="side-bar" style={props.issidebaropen ? {transform:`translateX(0)`,backgroundColor:"rgb(65, 64, 64)"}:{transform:`translate(-106px)`}}>
+            {/* <button className="Menu-close-icon" style={{display:props.menuclosebuttoncheck ? "":"none"}} onClick={(e)=>{e.stopPropagation(); props.MenuClose()}}><HighlightOffIcon/></button> */}
             <ul className="side-bar-listelements">
                 <li className="list-contents" style={clickStyle === true ? {backgroundColor:"#F59272",borderRadius:"0px 30px 30px 0px"}: {}} onClick={(e)=>{e.stopPropagation(),props.OpenNotes(), MainNotesStyle()}} >Notes <LightbulbIcon /></li>
                 <li className="list-contents" >Pins  <PushPinIcon /></li>
